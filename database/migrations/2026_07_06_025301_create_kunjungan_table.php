6c27af63d5de63bc5df35fcf17a96371d12867e7
Badrull <badrl.xrpl2017smkrus@gmail.com>
2026-07-06 09:59:09 +0700

    WIP modul kunjungan: migration

---
+++ database/migrations/2026_07_06_025301_create_kunjungan_table.php
@@ -0,0 +1,36 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('kunjungan', function (Blueprint $table) {
+            $table->id();
+            $table->integer('pasien_id');
+            $table->integer('dokter_id');
+            $table->foreign('pasien_id')->references('id')->on('pasien');
+            $table->foreign('dokter_id')->references('id')->on('dokter');
+            $table->date('tanggal');
+            $table->text('keluhan');
+            $table->string('diagnosis');
+            $table->decimal('biaya');
+            $table->string('status');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('kunjungan');
+    }
+};
